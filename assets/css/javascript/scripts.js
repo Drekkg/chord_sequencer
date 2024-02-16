dd39bb470162cba5c69aa955d5edc791c32e8a4a
drekkg <rizzagum@gmail.com>
2024-02-17 00:29:43 +0100

    FEAT adds selectKey function and ternary to select sharps

--- assets/css/javascript/scripts.js
+++ assets/css/javascript/scripts.js
@@ -39,8 +39,24 @@ function mainKeyDisplay(val) {
   let keyToDisplay = document.getElementById("select-key").textContent = val;
   selectKey();
 }
-
+/*Selects the appropriate scale according to the main Key selected **/
 function selectKey() {
-let selectedKey = document.getElementById("select-key").textContent.toLowerCase();
-console.log(selectedKey);
+  const notes = {
+    c: ['c4', 'c#4', 'd4', 'd#4', 'e4', 'f4', 'f#4', 'g4', 'g#4', 'a4', 'a#4', 'b4', 'c5'],
+    cshp: ['c#4', 'd4', 'd#4', 'e4', 'f4', 'f#4', 'g4', 'g#4', 'a4', 'a#4', 'b4', 'c5', 'c#5'],
+    d: ['d4', 'd#4', 'e4', 'f4', 'f#4', 'g4', 'g#4', 'a4', 'a#4', 'b4', 'c5', 'c#5', 'd5'],
+    dshp: ['d#4', 'e4', 'f4', 'f#4', 'g4', 'g#4', 'a4', 'a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5'],
+    e: ['e4', 'f4', 'f#4', 'g4', 'g#4', 'a4', 'a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5', 'e5'],
+    f: ['f4', 'f#4', 'g4', 'g#4', 'a4', 'a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5', 'e5', 'f5'],
+    fshp: ['f#4', 'g4', 'g#4', 'a4', 'a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5', 'e5', 'f5', 'f#5'],
+    g: ['g4', 'g#4', 'a4', 'a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5', 'e5', 'f5', 'f#5', 'g5'],
+    gshp: ['g#4', 'a4', 'a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5', 'e5', 'f5', 'f#5', 'g5', 'g#5'],
+    a: ['a4', 'a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5', 'e5', 'f5', 'f#5', 'g5', 'g#5', 'a5'],
+    ashp: ['a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5', 'e5', 'f5', 'f#5', 'g5', 'g#5', 'a5', 'a#5'],
+    b: ['b4', 'c5', 'c#5', 'd5', 'd#5', 'e5', 'f5', 'f#5', 'g5', 'g#5', 'a5', 'a#5', 'b5'],
+  };
+let mainKey = document.getElementById("select-key").textContent.toLowerCase();
+mainKey[1] === '#' ? mainKey = mainKey[0] + 'shp' : mainKey;
+let selectedKey = notes[mainKey];
+
 }
