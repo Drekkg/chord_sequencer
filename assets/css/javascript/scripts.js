4db8c0ae8357e599c141a3fd5e2f2b4a307af058
drekkg <rizzagum@gmail.com>
2024-02-16 16:09:58 +0100

    MAINT adds event listeners and functions to arrows

--- assets/css/javascript/scripts.js
+++ assets/css/javascript/scripts.js
@@ -2,17 +2,29 @@
  and assign event listeners */
 
 document.addEventListener("DOMContentLoaded", function () {
+  mainKey();
+  console.log("loaded")
+  let buttons = document.getElementsByTagName("button");
 
-    let buttons = document.getElementsByTagName("button");
-
-    for (let button of buttons) {
-      button.addEventListener("click", function () {
-          let modeType  = this.getAttribute("data-type");
-          console.log(modeType);
-      });
-    }
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      let modeType = this.getAttribute("data-type");
+      console.log(modeType);
+    });
+  }
 });
-   
-function mainKey () {
-  let keys = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b'];
+
+function mainKey() {
+  let keys = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"];
+  let up = document.getElementById("up-arrow");
+  let down = document.getElementById("down-arrow");
+  let keyIndex = 0;
+  up.addEventListener("click", function keyArrayUp() {
+    keyIndex ++;
+    console.log(keys[keyIndex]);
+  });
+  down.addEventListener("click", function keyArrayDown() {
+    keyIndex --;
+    console.log(keys[keyIndex]);
+  });
 }
