95d5f2b0c7e04b6c2de377baddf7bbf091c94580
drekkg <rizzagum@gmail.com>
2024-06-15 18:44:59 +0200

    FEAT adds 1/8th note note lengths

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -18,10 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       /*set selected mode to localStorage */
       const chordSequencerMode = modeType;
-      localStorage.setItem(
-        "chord_sequencer_mode",
-        JSON.stringify(chordSequencerMode)
-      );
+      localStorage.setItem("chord_sequencer_mode", JSON.stringify(chordSequencerMode));
       modeTypefilter(modeType);
     });
   }
@@ -52,7 +49,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 /*Selects the note length from an array and displays the note length on the page.**/
 function noteLength() {
-  const noteLengthArray = ["1/1", "1/2", "1/4", "1/16"];
+  const noteLengthArray = ["1/1", "1/2", "1/4", "1/8", "1/16"];
   let noteLengthSelected = document.getElementById("select-note-length");
   noteLengthSelected.textContent = noteLengthArray[2];
 
@@ -65,10 +62,8 @@ function noteLength() {
 
   /*adds event listener to the note length button and changes the note length on click */
   noteLengthSelected.addEventListener("click", () => {
-    let noteLengthIndex = noteLengthArray.indexOf(
-      noteLengthSelected.textContent
-    );
-    if (noteLengthIndex < 3) {
+    let noteLengthIndex = noteLengthArray.indexOf(noteLengthSelected.textContent);
+    if (noteLengthIndex < 4) {
       noteLengthIndex++;
     } else {
       noteLengthIndex = 0;
@@ -77,10 +72,7 @@ function noteLength() {
     const chord_sequencer_note = noteLengthArray[noteLengthIndex];
 
     /*set selected note to localStorage */
-    localStorage.setItem(
-      "chord_sequencer_note",
-      JSON.stringify(chord_sequencer_note)
-    );
+    localStorage.setItem("chord_sequencer_note", JSON.stringify(chord_sequencer_note));
   });
 }
 
@@ -127,7 +119,7 @@ function mainKeyDisplay(mainKeyValue) {
   localStorage.setItem("chord_sequencer_key", JSON.stringify(mainKeyValue));
   modeTypefilter();
 }
-/*selects the dinotes for the mode type and displays the mode type on the page **/
+/*selects the notes for the mode type and displays the mode type on the page **/
 function modeTypefilter(modeType) {
   let modes = {
     ionian: [0, 2, 4, 5, 7, 9, 11],
@@ -151,7 +143,7 @@ function modeTypefilter(modeType) {
     modeType = modes.ionian;
   }
 
-  /* read nodeType from localStorage  chord_sequencer_node */
+  /* read modeType from localStorage  chord_sequencer_mode */
   if (localStorage.getItem("chord_sequencer_mode")) {
     modeName = JSON.parse(localStorage.getItem("chord_sequencer_mode"));
     modeType = modes[modeName];
@@ -164,19 +156,89 @@ function modeTypefilter(modeType) {
 /*Selects the appropriate scale according to the main Key selected **/
 function selectKey(modeType, modeName) {
   const notes = {
-  c: ['c-4', 'c#4', 'd-4', 'd#4', 'e-4', 'f-4', 'f#4', 'g-4', 'g#4', 'a-4', 'a#4', 'b-4', 'c-5'],
-cshp: ['c#4', 'd-4', 'd#4', 'e-4', 'f-4', 'f#4', 'g-4', 'g#4', 'a-4', 'a#4', 'b-4', 'c-5', 'c#5'],
-d: ['d-4', 'd#4', 'e-4', 'f-4', 'f#4', 'g-4', 'g#4', 'a-4', 'a#4', 'b-4', 'c-5', 'c#5', 'd-5'],
-dshp: ['d#4', 'e-4', 'f-4', 'f#4', 'g-4', 'g#4', 'a-4', 'a#4', 'b-4', 'c-5', 'c#5', 'd-5', 'd#5'],
-e: ['e-4', 'f-4', 'f#4', 'g-4', 'g#4', 'a-4', 'a#4', 'b-4', 'c-5', 'c#5', 'd-5', 'd#5', 'e-5'],
-f: ['f-4', 'f#4', 'g-4', 'g#4', 'a-4', 'a#4', 'b-4', 'c-5', 'c#5', 'd-5', 'd#5', 'e-5', 'f-5'],
-fshp: ['f#4', 'g-4', 'g#4', 'a-4', 'a#4', 'b-4', 'c-5', 'c#5', 'd-5', 'd#5', 'e-5', 'f-5', 'f#5'],
-g: ['g-4', 'g#4', 'a-4', 'a#4', 'b-4', 'c-5', 'c#5', 'd-5', 'd#5', 'e-5', 'f-5', 'f#5', 'g-5'],
-gshp: ['g#4', 'a-4', 'a#4', 'b-4', 'c-5', 'c#5', 'd-5', 'd#5', 'e-5', 'f-5', 'f#5', 'g-5', 'g#5'],
-a: ['a-4', 'a#4', 'b-4', 'c-5', 'c#5', 'd-5', 'd#5', 'e-5', 'f-5', 'f#5', 'g-5', 'g#5', 'a-5'],
-ashp: ['a#4', 'b-4', 'c-5', 'c#5', 'd-5', 'd#5', 'e-5', 'f-5', 'f#5', 'g-5', 'g#5', 'a-5', 'a#5'],
-b: ['b-4', 'c-5', 'c#5', 'd-5', 'd#5', 'e-5', 'f-5', 'f#5', 'g-5', 'g#5', 'a-5', 'a#5', 'b-5']
-};
+    c: ["c-4", "c#4", "d-4", "d#4", "e-4", "f-4", "f#4", "g-4", "g#4", "a-4", "a#4", "b-4", "c-5"],
+    cshp: [
+      "c#4",
+      "d-4",
+      "d#4",
+      "e-4",
+      "f-4",
+      "f#4",
+      "g-4",
+      "g#4",
+      "a-4",
+      "a#4",
+      "b-4",
+      "c-5",
+      "c#5",
+    ],
+    d: ["d-4", "d#4", "e-4", "f-4", "f#4", "g-4", "g#4", "a-4", "a#4", "b-4", "c-5", "c#5", "d-5"],
+    dshp: [
+      "d#4",
+      "e-4",
+      "f-4",
+      "f#4",
+      "g-4",
+      "g#4",
+      "a-4",
+      "a#4",
+      "b-4",
+      "c-5",
+      "c#5",
+      "d-5",
+      "d#5",
+    ],
+    e: ["e-4", "f-4", "f#4", "g-4", "g#4", "a-4", "a#4", "b-4", "c-5", "c#5", "d-5", "d#5", "e-5"],
+    f: ["f-4", "f#4", "g-4", "g#4", "a-4", "a#4", "b-4", "c-5", "c#5", "d-5", "d#5", "e-5", "f-5"],
+    fshp: [
+      "f#4",
+      "g-4",
+      "g#4",
+      "a-4",
+      "a#4",
+      "b-4",
+      "c-5",
+      "c#5",
+      "d-5",
+      "d#5",
+      "e-5",
+      "f-5",
+      "f#5",
+    ],
+    g: ["g-4", "g#4", "a-4", "a#4", "b-4", "c-5", "c#5", "d-5", "d#5", "e-5", "f-5", "f#5", "g-5"],
+    gshp: [
+      "g#4",
+      "a-4",
+      "a#4",
+      "b-4",
+      "c-5",
+      "c#5",
+      "d-5",
+      "d#5",
+      "e-5",
+      "f-5",
+      "f#5",
+      "g-5",
+      "g#5",
+    ],
+    a: ["a-4", "a#4", "b-4", "c-5", "c#5", "d-5", "d#5", "e-5", "f-5", "f#5", "g-5", "g#5", "a-5"],
+    ashp: [
+      "a#4",
+      "b-4",
+      "c-5",
+      "c#5",
+      "d-5",
+      "d#5",
+      "e-5",
+      "f-5",
+      "f#5",
+      "g-5",
+      "g#5",
+      "a-5",
+      "a#5",
+    ],
+    b: ["b-4", "c-5", "c#5", "d-5", "d#5", "e-5", "f-5", "f#5", "g-5", "g#5", "a-5", "a#5", "b-5"],
+  };
 
   let mainKey = document.getElementById("select-key").textContent.toLowerCase();
   if (mainKey[1] === "#") {
@@ -227,11 +289,8 @@ function filterScale(scale, mode, modeName) {
 /*Displays the mode chords on the page **/
 function displayModeChords(filteredModeChords, modeNameToFilter) {
   for (let i = 0; i < filteredModeChords.length; i++) {
-    document.getElementById(`deg${i + 1}`).innerHTML =
-      filteredModeChords[i].toUpperCase();
-    document.getElementById(
-      `deg${i + 1}`
-    ).classList = `button ${modeNameToFilter[i]}`;
+    document.getElementById(`deg${i + 1}`).innerHTML = filteredModeChords[i].toUpperCase();
+    document.getElementById(`deg${i + 1}`).classList = `button ${modeNameToFilter[i]}`;
   }
 }
 
@@ -253,33 +312,32 @@ function synth() {
 
   /*An object containing the arrays that will map to the correct notes to be played  */
   const notes = {
-  'c-4': ['c4', 'c#4', 'd4', 'd#4', 'e4', 'f4', 'f#4', 'g4', 'g#4', 'a4', 'a#4', 'b4', 'c5'],
-  'c4shp': ['c#4', 'd4', 'd#4', 'e4', 'f4', 'f#4', 'g4', 'g#4', 'a4', 'a#4', 'b4', 'c5', 'c#5'],
-  'd-4': ['d4', 'd#4', 'e4', 'f4', 'f#4', 'g4', 'g#4', 'a4', 'a#4', 'b4', 'c5', 'c#5', 'd5'],
-  'd4shp': ['d#4', 'e4', 'f4', 'f#4', 'g4', 'g#4', 'a4', 'a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5'],
-  'e-4': ['e4', 'f4', 'f#4', 'g4', 'g#4', 'a4', 'a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5', 'e5'],
-  'f-4': ['f4', 'f#4', 'g4', 'g#4', 'a4', 'a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5', 'e5', 'f5'],
-  'f4shp': ['f#4', 'g4', 'g#4', 'a4', 'a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5', 'e5', 'f5', 'f#5'],
-  'g-4': ['g4', 'g#4', 'a4', 'a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5', 'e5', 'f5', 'f#5', 'g5'],
-  'g4shp': ['g#4', 'a4', 'a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5', 'e5', 'f5', 'f#5', 'g5', 'g#5'],
-  'a-4': ['a4', 'a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5', 'e5', 'f5', 'f#5', 'g5', 'g#5', 'a5'],
-  'a4shp': ['a#4', 'b4', 'c5', 'c#5', 'd5', 'd#5', 'e5', 'f5', 'f#5', 'g5', 'g#5', 'a5', 'a#5'],
-  'b-4': ['b4', 'c5', 'c#5', 'd5', 'd#5', 'e5', 'f5', 'f#5', 'g5', 'g#5', 'a5', 'a#5', 'b5'],
-  'c-5': ['c5', 'c#5', 'd5', 'd#5', 'e5', 'f5', 'f#5', 'g5', 'g#5', 'a5', 'a#5', 'b5', 'c6'],
-  'c5shp': ['c#5', 'd5', 'd#5', 'e5', 'f5', 'f#5', 'g5', 'g#5', 'a5', 'a#5', 'b5', 'c6', 'c#6'],
-  'd-5': ['d5', 'd#5', 'e5', 'f5', 'f#5', 'g5', 'g#5', 'a5', 'a#5', 'b5', 'c6', 'c#6', 'd6'],
-  'd5shp': ['d#5', 'e5', 'f5', 'f#5', 'g5', 'g#5', 'a5', 'a#5', 'b5', 'c6', 'c#6', 'd6', 'd#6'],
-  'e-5': ['e5', 'f5', 'f#5', 'g5', 'g#5', 'a5', 'a#5', 'b5', 'c6', 'c#6', 'd6', 'd#6', 'e6'],
-  'f-5': ['f5', 'f#5', 'g5', 'g#5', 'a5', 'a#5', 'b5', 'c6', 'c#6', 'd6', 'd#6', 'e6', 'f6'],
-  'f5shp': ['f#5', 'g5', 'g#5', 'a5', 'a#5', 'b5', 'c6', 'c#6', 'd6', 'd#6', 'e6', 'f6', 'f#6'],
-  'g-5': ['g5', 'g#5', 'a5', 'a#5', 'b5', 'c6', 'c#6', 'd6', 'd#6', 'e6', 'f6', 'f#6', 'g6'],
-  'g5shp': ['g#5', 'a5', 'a#5', 'b5', 'c6', 'c#6', 'd6', 'd#6', 'e6', 'f6', 'f#6', 'g6', 'g#6'],
-  'a-5': ['a5', 'a#5', 'b5', 'c6', 'c#6', 'd6', 'd#6', 'e6', 'f6', 'f#6', 'g6', 'g#6', 'a6'],
-  'a5shp': ['a#5', 'b5', 'c6', 'c#6', 'd6', 'd#6', 'e6', 'f6', 'f#6', 'g6', 'g#6', 'a6', 'a#6'],
-  'b-5': ['b5', 'c6', 'c#6', 'd6', 'd#6', 'e6', 'f6', 'f#6', 'g6', 'g#6', 'a6', 'a#6', 'b6']
-};
+    "c-4": ["c4", "c#4", "d4", "d#4", "e4", "f4", "f#4", "g4", "g#4", "a4", "a#4", "b4", "c5"],
+    c4shp: ["c#4", "d4", "d#4", "e4", "f4", "f#4", "g4", "g#4", "a4", "a#4", "b4", "c5", "c#5"],
+    "d-4": ["d4", "d#4", "e4", "f4", "f#4", "g4", "g#4", "a4", "a#4", "b4", "c5", "c#5", "d5"],
+    d4shp: ["d#4", "e4", "f4", "f#4", "g4", "g#4", "a4", "a#4", "b4", "c5", "c#5", "d5", "d#5"],
+    "e-4": ["e4", "f4", "f#4", "g4", "g#4", "a4", "a#4", "b4", "c5", "c#5", "d5", "d#5", "e5"],
+    "f-4": ["f4", "f#4", "g4", "g#4", "a4", "a#4", "b4", "c5", "c#5", "d5", "d#5", "e5", "f5"],
+    f4shp: ["f#4", "g4", "g#4", "a4", "a#4", "b4", "c5", "c#5", "d5", "d#5", "e5", "f5", "f#5"],
+    "g-4": ["g4", "g#4", "a4", "a#4", "b4", "c5", "c#5", "d5", "d#5", "e5", "f5", "f#5", "g5"],
+    g4shp: ["g#4", "a4", "a#4", "b4", "c5", "c#5", "d5", "d#5", "e5", "f5", "f#5", "g5", "g#5"],
+    "a-4": ["a4", "a#4", "b4", "c5", "c#5", "d5", "d#5", "e5", "f5", "f#5", "g5", "g#5", "a5"],
+    a4shp: ["a#4", "b4", "c5", "c#5", "d5", "d#5", "e5", "f5", "f#5", "g5", "g#5", "a5", "a#5"],
+    "b-4": ["b4", "c5", "c#5", "d5", "d#5", "e5", "f5", "f#5", "g5", "g#5", "a5", "a#5", "b5"],
+    "c-5": ["c5", "c#5", "d5", "d#5", "e5", "f5", "f#5", "g5", "g#5", "a5", "a#5", "b5", "c6"],
+    c5shp: ["c#5", "d5", "d#5", "e5", "f5", "f#5", "g5", "g#5", "a5", "a#5", "b5", "c6", "c#6"],
+    "d-5": ["d5", "d#5", "e5", "f5", "f#5", "g5", "g#5", "a5", "a#5", "b5", "c6", "c#6", "d6"],
+    d5shp: ["d#5", "e5", "f5", "f#5", "g5", "g#5", "a5", "a#5", "b5", "c6", "c#6", "d6", "d#6"],
+    "e-5": ["e5", "f5", "f#5", "g5", "g#5", "a5", "a#5", "b5", "c6", "c#6", "d6", "d#6", "e6"],
+    "f-5": ["f5", "f#5", "g5", "g#5", "a5", "a#5", "b5", "c6", "c#6", "d6", "d#6", "e6", "f6"],
+    f5shp: ["f#5", "g5", "g#5", "a5", "a#5", "b5", "c6", "c#6", "d6", "d#6", "e6", "f6", "f#6"],
+    "g-5": ["g5", "g#5", "a5", "a#5", "b5", "c6", "c#6", "d6", "d#6", "e6", "f6", "f#6", "g6"],
+    g5shp: ["g#5", "a5", "a#5", "b5", "c6", "c#6", "d6", "d#6", "e6", "f6", "f#6", "g6", "g#6"],
+    "a-5": ["a5", "a#5", "b5", "c6", "c#6", "d6", "d#6", "e6", "f6", "f#6", "g6", "g#6", "a6"],
+    a5shp: ["a#5", "b5", "c6", "c#6", "d6", "d#6", "e6", "f6", "f#6", "g6", "g#6", "a6", "a#6"],
+    "b-5": ["b5", "c6", "c#6", "d6", "d#6", "e6", "f6", "f#6", "g6", "g#6", "a6", "a#6", "b6"],
+  };
 
-   
   /*An object containing the arrays that will map to the correct shape of the chords to be played */
   const shapes = {
     maj: [0, 4, 7],
@@ -319,12 +377,11 @@ and triggers the sound */
       "1/1": "1n",
       "1/2": "2n",
       "1/4": "4n",
+      "1/8": "8n",
       "1/16": "16n",
     };
     let selectedNoteLengthtoPlay =
-      noteLengthObject[
-        document.getElementById("select-note-length").textContent
-      ];
+      noteLengthObject[document.getElementById("select-note-length").textContent];
     if (Tone.context.state != "running") {
       Tone.start();
     }
@@ -332,4 +389,3 @@ and triggers the sound */
     synth.triggerAttackRelease(note, selectedNoteLengthtoPlay);
   }
 }
-
